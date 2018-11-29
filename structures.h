2c15355c41250610bb02f81ec2b0509dfe272194
MoNazar <44373397+MoNazar@users.noreply.github.com>
2018-11-29 16:46:44 +0100

    Create structures.h

---
+++ structures.h
@@ -0,0 +1,15 @@
+// Here are preliminary structures for a city and a node of elastic net.
+
+#pragma once
+#include <iostream>
+
+struct City {
+
+	int x; // koordinate x 
+	int y; // koordinate y 
+};
+
+struct NodeElasticNet {
+
+	char node = '*';
+};
